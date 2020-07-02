0e0ca0460d8f822c3924b8b3f0ac83b3c3e0472f
Augusto Santos <augustosantoscordeiro@gmail.com>
2020-07-02 15:31:19 -0300

    Projeto iniciado, instanciando e imprimindo uma classe Order que faz uso de enums

---
+++ Course_9_1/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using Course_9_1.Entities.Enums;
+using System;
+
+namespace Course_9_1.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", "
+                + Moment + ", "
+                + Status;
+        }
+    }
+}
